4a424a59dc79bb31a79546a622cea5b403a2f5c5
John Doe <jch@gudisoft.co.kr>
2020-09-04 11:56:51 +0900

    set/add actor to MovieDetail

--- client/src/components/views/MovieDetail/MovieDetail.jsx
+++ client/src/components/views/MovieDetail/MovieDetail.jsx
@@ -3,10 +3,14 @@ import { useEffect } from "react";
 import { BASE_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
 import MainImage from "../LandingPage/Sections/MainImage";
 import MovieInfo from "./Sections/MovieInfo";
+import GridCards from "../common/GridCards";
+import { Row } from "antd";
 
 function MovieDetail(props) {
   let movieId = props.match.params.movieId;
   const [Movie, setMovie] = useState(null);
+  const [Casts, setCasts] = useState(null);
+  const [ActorToggle, setActorToggle] = useState(false);
 
   useEffect(() => {
     fetch(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=ko`)
@@ -14,10 +18,22 @@ function MovieDetail(props) {
         return res.json();
       })
       .then((data) => {
-        console.log(data);
         setMovie(data);
       });
+
+    fetch(`${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}&language=ko`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setCasts(data.cast);
+      });
   }, []);
+
+  const toggleHandler = () => {
+    setActorToggle(!ActorToggle);
+  };
 
   return (
     <div>
@@ -51,8 +67,28 @@ function MovieDetail(props) {
         <div
           style={{ display: "flex", justifyContent: "center", margin: "2rem" }}
         >
-          <button>Toggle Actor View</button>
+          <button onClick={toggleHandler}>Toggle Actor View</button>
         </div>
+
+        {ActorToggle && (
+          <Row gutter={[16, 16]}>
+            {Casts &&
+              Casts.map((cast, i) => {
+                return (
+                  <div key={i}>
+                    <GridCards
+                      image={
+                        cast.profile_path
+                          ? `${IMAGE_BASE_URL}/w500/${cast.profile_path}`
+                          : null
+                      }
+                      characterName={cast.name}
+                    />
+                  </div>
+                );
+              })}
+          </Row>
+        )}
       </div>
     </div>
   );
--- client/src/components/views/common/GridCards.jsx
+++ client/src/components/views/common/GridCards.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { Col } from "antd";
 
-function GridCards({ image, movieId, movieName }) {
+function GridCards({ image, movieId, movieName, characterName }) {
   return (
     <Col lg={6} md={8} xs={24}>
       <div style={{ position: "relative" }}>
-        <a href={`/movie/${movieId}`}>
+        {movieId ? (
+          <a href={`/movie/${movieId}`}>
+            <img
+              style={{ width: "100%", height: "320px" }}
+              src={image}
+              alt={movieName}
+            />
+          </a>
+        ) : (
           <img
             style={{ width: "100%", height: "320px" }}
             src={image}
-            alt={movieName}
+            alt={characterName}
           />
-        </a>
+        )}
       </div>
     </Col>
   );
